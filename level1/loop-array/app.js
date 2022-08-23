76654612c5a26fbc8700ebf879845ca85ef9d852
Madison Selfaison <madisonselfaison@gmail.com>
2022-08-23 15:38:18 -0600

    A work in progress for my loop-array assignment

--- level1/loop-array/app.js
+++ level1/loop-array/app.js
@@ -1,1 +1,37 @@
-console.log("hello")
+//Loop through the following array and count how many "`computer`s" there are. Log the final count:
+
+var officeItems = ["stapler", "monitor", "computer", "desk", "lamp", "computer", "lamp", "stapler", "computer",  "computer"]
+for (var i = 0; i<computer i++){
+    console.log(computer.length)
+}
+
+
+//Loop through the following array and log to the console "old enough" if they are 18 or older, and "not old enough" if they aren't 18.//
+
+var users = [
+    {
+      name: "Mike",
+      age: 12,
+      gender: "male"
+    },{
+      name: "Madeline",
+      age: 80,
+      gender: "female"
+    },{
+      name: "Cheryl",
+      age: 22,
+      gender: "female"
+    },{
+      name: "Sam",
+      age: 30,
+      gender: "male"
+    },{
+      name: "Suzy",
+      age: 4,
+      gender: "female"
+    }
+  ]
+
+  for(var i = 18; i < 100; i++){
+    console.log("Hi, I am " + users[i].name + " and I am old enough")
+  }
