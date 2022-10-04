6e8fc914ca937fa316e0781b265b004edeb3f1c7
Madison Selfaison <madisonselfaison@gmail.com>
2022-10-04 14:09:03 -0600

    updated v school assignments

--- level2/capstone-axios-todo-project/app.js
+++ level2/capstone-axios-todo-project/app.js
@@ -1,180 +1,90 @@
-const baseURL = "https://api.vschool.io/madisonselfaison/todo/"
-const form = document.querySelector('form')
-const submit = document.querySelector('button')
-const listWrapper = document.getElementById('list-wrapper')
-const imageUrl = document.getElementById('img-url')
-const formImage = document.getElementById('form-image')
-getTodos()
+const todoForm = document["mainForm"]
+//console.log(mainForm)
 
-
-function postTodo() {
-    if (form.title.value) {
-        let newPackage = new Package()
-        axios.post(baseURL, newPackage).then(response => renderResponse(response.data))
-        form.reset()
-        formImage.src=""
-        form.title.focus()
-    } else {
-        alert("A title is required to submit this todo.")
-        form.title.focus()
-    }
+function getData(){
+    axios.get("https://api.vschool.io/madisonselfaison/todo/")
+    .then(response => listData(response.data))
+    .catch(error => error)
 }
+getData()
 
-function getTodos() {
-    axios.get(baseURL).then(loopandRenderTodos)
-}
+function listData(arr){
+    clearList()
+    for(let i = 0; i < arr.length; i++){
+        const h1 = document.createElement('h1')
+        h1.textContent = arr[i].title
+        document.getElementById("todoList").appendChild(h1)
 
-function loopandRenderTodos(response) {
-    response.data.forEach(todoObject => renderResponse(todoObject))
-}
-
-function package() {
-    this.title = form.title.value;
-    this.description = form.desc.value;
-    this.price = form.price.value;
-    this.imgUrl = formImage.value;
-    this.completed = form.completed.checked;
-}
-
-function renderResponse(response) {
-    let rendered = new RenderObject(response)
-    let itemWrapper = document.createElement('div')
-    itemWrapper.classList = "item-wrapper"
-    itemWrapper.id = rendered.id
-    let number = document.createElement('p')
-    number.classList = "number"
-    number.textContent = String(listWrapper.children.length + 1) + "."
-    let deleteX = document.createElement('p')
-    deleteX.classList = "deleteX"
-    deleteX.textContent = "X"
-    deleteX.addEventListener("click", deleteTodo)
-    itemWrapper.appendChild(number)
-    itemWrapper.appendChild(deleteX)
-
-    for (key in rendered) {
-        let item = document.createElement(rendered[key].elementType)
-        if (rendered[key].elementType === "img"){
-            item.classList = rendered[key].class
-            item.src = rendered[key].value
-        } else {
-        item.classList = rendered[key].class
-        item.textContent = rendered[key].value
-        if (rendered[key].holder !== undefined) {
-            item.value = rendered[key].holder
-            item.addEventListener("click", updateTodo)
-        }
-    }
-        itemWrapper.appendChild(item)
-    }
-    listWrapper.appendChild(itemWrapper)
-
-
-}
-
-function renderObject(obj) {
-    this.title = {
-        elementType: "h3",
-        value: obj.title,
-        class: "todo-title"
-    }
-    this.description = {
-        elementType: "p",
-        value: obj.description,
-        class: "todo-desc"
-    }
-    this.price = {
-        elementType: "p",
-        value: obj.price,
-        class: "todo-title"
-    }
-    this.imgUrl = {
-        elementType: "img",
-        value: obj.imgUrl,
-        class: "todo-image"
-    }
-    this.completed = {
-        elementType: "p",
-        value: obj.completed ? "✓ Completed" : "X Not Complete",
-        class: (obj.completed ? "green" : "red") + " complete-check",
-        holder: obj.completed ? true : false,
-
-    }
-    this.id = obj._id;
-}
-
-
-
-submit.addEventListener("click", postTodo)
-
-imageUrl.addEventListener("click", (e) => {
-    navigator.clipboard.readText().then(response => {
-        formImage.src = response
-        formImage.value = response
+    const delBtn = document.createElement("button")
+    delBtn.textContent = "Delete To-Do"
+    h1.appendChild(delBtn)
+    delBtn.addEventListener("click", ()=>{
+        axios.delete("https://api.vschool.io/madisonselfaison/todo/" + arr[i]._id)
+        .then(getData)
     })
-});
+    const doneBtn = document.createElement("button")
+    doneBtn.textContent = "Done"
+    h1.appendChild(doneBtn)
 
-function deleteTodo(e) {
-    const id = e.target.parentNode.id
-    e.target.parentNode.remove()
-    axios.delete(baseURL + id).then(changeItemIndex, () => alert("There was a problem deleting your todo :("));
-}
-
-function updateTodo(e) {
-    let id = e.target.parentNode.id
-    let update = {
-        completed: !e.target.value
+    const updates = {
+        completed: true
     }
-    axios.put(baseURL + id, update).then(response => {
-        if (response.data.completed) {
-            e.target.classList = "green complete-check"
-            e.target.value = true
-            e.target.textContent = "✓ Completed"
-        } else {
-            e.target.classList = "red complete-check"
-            e.target.value = false
-            e.target.textContent = "X Not Complete"
-        }
-
-        (response.data.completed ? "green" : "red") + " complete-check"
-    });
-
-}
-
-function changeItemIndex() {
-    for (i = 0; i < listWrapper.children.length + 1; i++) {
-        if (listWrapper.childNodes[i].childNodes[0]) {
-            listWrapper.childNodes[i].childNodes[0].textContent = i + "."
-        }
+    const otherUpdates = {
+        completed: false
     }
-}
-
-
-// const baseURL = "https://api.vschool.io/madisonselfaison/todo/"
-// const form = document.querySelector('form')
-// const submit = document.querySelector('button')
-// const list = document.getElementById('list-wrapper')
-// const imgUrl = document.getElementById('img-url')
-// const imgForm = document.getElementById('form-image')
-// getTodos()
-
-// const todoList = document.getElementById("todo-list")
-
-// function makeTodo(todo){
-//     // create/select the element
-//     const container = document.createElement("div")
-//     const title = document.createElement("h1")
-//     const deleteButton = document.createElement("button")
-//     // edit element
-//     title.textContent = todo.title
-//     title.className = "title"
-//     deleteButton.textContent = "delete"
-//     // append
-//     todoList.appendChild(container)
-//     container.appendChild(title)
-//     container.appendChild(deleteButton)
+    doneBtn.addEventListener('click', ()=> {
+        if(arr[i].completed === false){
+            axios.put("https://api.vschool.io/madisonselfaison/todo/" + arr[i]._id, updates)
+            .then(getData)
+            .catch(error => console.log(error))
+        } else if (arr[i].completed === true){
+            axios.put("https://api.vschool.io/madisonselfaison/todo/" + arr[i]._id, otherUpdates)
+            .then(getData)
+            .catch(error => console.log(error))
+        }
+    })
+    const descriptionh2 = document.createElement('h2')
+    descriptionh2.textContent = arr[i].description
+    document.getElementById("todoList").appendChild(descriptionh2)
+    const img = document.createElement('img')
+    img.src = arr[i].imgUrl
+    img.style.width = '90px'
+    img.style.height = '90px'
+    document.getElementById("todoList").appendChild(img)
     
-//     deleteButton.addEventListener("click", (e)=>{
-//         console.dir(e.target)
-//         parent.removeChild()
-//     })
-// }
+    const priceh2 = document.createElement('h2')
+    priceh2.textContent = arr[i].price
+    document.getElementById("todoList").appendChild(priceh2)
+    
+    if (arr[i].completed === true){
+    h1.style.textDecoration = 'line-through';
+    descriptionh2.style.textDecoration = 'line-through';
+    priceh2.style.textDecoration = 'line-through';
+    }
+    }
+}
+
+function clearList (){
+const el = document.getElementById("todoList")
+while(el.firstChild){
+    el.removeChild(el.firstChild)
+}
+}
+
+todoForm.addEventListener("submit", function(e){
+e.preventDefault()
+const newTodo = {
+    title: todoForm.title.value,
+    description: todoForm.description.value,
+    price: todoForm.price.value,
+    imgUrl: todoForm.imgUrl.value
+}
+
+todoForm.title.value=""
+todoForm.description.value=""
+todoForm.price.value=""
+todoForm.imgUrl.value=""
+axios.post("https://api.vschool.io/madisonselfaison/todo/", newTodo)
+    .then(getData)
+    .catch(error => console.log(error))
+})
